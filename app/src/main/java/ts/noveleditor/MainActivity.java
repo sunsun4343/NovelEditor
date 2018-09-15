5542644d24e61a699c44237c2c44d3d974ecb9e5
sunsun <ss.lee@morph.kr>
2018-09-15 19:28:20 +0900

    프로젝트 생성

---
+++ app/src/main/java/ts/noveleditor/MainActivity.java
@@ -0,0 +1,23 @@
+package ts.noveleditor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //소설 다운로드 기능
+        //번역 기능
+        //발음기호 표시기능
+        //맞춤법 검사기능
+        //추가 후처리 번역 헬퍼
+        
+
+
+
+    }
+}
